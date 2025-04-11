73468a01b1f46b259193911f7eaaf0436964ada6
Douglas Martins <douglasfabiamartins@hotmail.com>
2025-04-10 23:25:50 -0300

    remove unecessary env variables from clerk

--- src/env.js
+++ src/env.js
@@ -16,8 +16,6 @@ export const env = createEnv({
     SINGLESTORE_USER: z.string(),
     SINGLESTORE_PASSWORD: z.string(),
     SINGLESTORE_DATABASE: z.string(),
-    CLERK_PUBLISHABLE_KEY: z.string(),
-    CLERK_SECRET_KEY: z.string(),
   },
 
   /**
@@ -41,8 +39,6 @@ export const env = createEnv({
     SINGLESTORE_USER: process.env.SINGLESTORE_USER,
     SINGLESTORE_PASSWORD: process.env.SINGLESTORE_PASSWORD,
     SINGLESTORE_DATABASE: process.env.SINGLESTORE_DATABASE,
-    CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
